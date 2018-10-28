e33302f4ea3e39349ff37f2119a7aa2c3f94e43d
Enzo Barrett <enzobarrett@gmail.com>
2018-10-28 01:15:16 -0600

    General fixes!

--- app.py
+++ app.py
@@ -6,9 +6,9 @@ app = Flask(__name__)
 CORS(app)
 random = random.SystemRandom()
 mydb = mysql.connector.connect(
-    host='localhost', 
-    user='enzo', 
-    passwd='password', 
+    host='localhost',
+    user='enzo',
+    passwd='password',
     database='highscores'
 )
 
@@ -25,6 +25,7 @@ def get_table(key):
     tablecursor = mydb.cursor()
     tablecursor.execute('SELECT name FROM keytables WHERE apikey = %s', (key,))
     tablename = tablecursor.fetchall()
+    mydb.commit()
     table = 'e'
     for row in tablename:
         table = row[0]
@@ -33,76 +34,52 @@ def get_table(key):
         return 'e'
     else:
         tablecursor.close()
-        return table
-def get_table2(key):
-    tablecursor2 = mydb.cursor()
-    tablecursor2.execute('SELECT name FROM keytables WHERE apikey = %s', (key,))
-    tablename = tablecursor2.fetchall()
-    table = 'e'
-    for row in tablename:
-        table = row[0]
-    if (table == 'e'):
-        tablecursor2.close()
-        return 'e'
-    else:
-        tablecursor.close()
-        return table
+        return tablesubstring
+
 @app.route('/')
 def gethighscorelist():
-    #mycursor.execute('SELECT * FROM scores ORDER BY CAST(score AS unsigned) DESC limit 10')
-    #row_headers = [ x[0] for x in mycursor.description ]
-    #json_data = []
-    #for result in rv:
-    #   json_data.append(dict(zip(row_headers, result)))
-
-    #return json.dumps(json_data)
-    getTable = get_table(request.args['key'])
-    print(getTable)
-    if (getTable == 'e'):
-        return Response("Error: not found", status=404)
-    else:
-        getcursor = mydb.cursor()
-        getcursor.execute('SELECT * FROM %s ORDER BY CAST(score AS unsigned) DESC limit 5' % (getTable,))
-        topscores = getcursor.fetchall()
-        print("fetch get scores")
-        row_headers = [ x[0] for x in getcursor.description ]
-        getcursor.close()
-        json_data = []
-        for result in topscores:
-            json_data.append(dict(zip(row_headers, result)))
-        return json.dumps(json_data)
-        #return str(paid(getTable))
+    print("here1")
+    getcursor = mydb.cursor()
+    getstring = 'SELECT * FROM ' + request.args['key'] + ' ORDER BY CAST(score AS unsigned) DESC limit 5'
+    getcursor.execute(getstring)
+    topscores = getcursor.fetchall()
+    print("fetch get scores")
+    field_names = [i[0] for i in getcursor.description]
+    getcursor.close()
+    json_data = []
+    for result in topscores:
+        json_data.append(dict(zip(field_names, result)))
+    print("about to return")
+    return Response(json.dumps(json_data), status=200)
+    #return str(paid(getTable))
 @app.route('/gettop')
 def gettop():
-    getTable = get_table(request.args['key'])
     topcursor = mydb.cursor()
-    topcursor.execute('select count(*) from %s' % (getTable))
+    topcursor.execute('select count(*) from %s' % (request.args['key'],))
     rv2 = topcursor.fetchall()
     topcursor.close()
     for row in rv2:
         numberintable = row[0]
         print(numberintable)
     if (numberintable < 5):
-        return "goodtogo" 
+        return "goodtogo"
     if (numberintable >= 5):
         usenumber = 4
     nthcursor = mydb.cursor()
-    nthcursor.execute('select score from %s order by cast(score as unsigned) desc limit %s,1;' % (getTable, usenumber,))
+    nthstring = 'select score from ' + request.args['key'] + ' order by cast(score as unsigned) desc limit ' + str(usenumber) + ',1;'
+    nthcursor.execute(nthstring)
     rv3 = nthcursor.fetchall()
     nthcursor.close()
     for row in rv3:
         return row[0]
 @app.route('/insertscore')
 def insert_score():
-    getTable = get_table2(request.args['key'])
-    if (getTable == 'e'):
-        return Response("Error: not found", status=404)
-    else:
-        insertcursor = mydb.cursor()
-        insertcursor.execute('insert into %s' % getTable + ' values (%s,%s)', (request.args['name'], request.args['score'],))
-        mydb.commit()
-        insertcursor.close()
-        return Response("Success", status=200)
+    insertcursor = mydb.cursor()
+    string = 'insert into ' + request.args['key'] + ' values (%s,%s)'
+    insertcursor.execute(string, (request.args['name'], request.args['score'],))
+    insertcursor.close()
+    mydb.commit()
+    return Response("success", status=200)
 @app.route('/getnewkey')
 def api_keygen():
 
@@ -121,7 +98,6 @@ def api_keygen():
             return True
         else:
             return False
-        
     apikey = ''.join(random.choice(string.lowercase) for x in range(5))
     tabname = ''.join(random.choice(string.lowercase) for x in range(5))
     while checkduplicate(apikey, tabname):
@@ -135,5 +111,4 @@ def api_keygen():
     addkeycursor.close()
     return apikey
 if __name__ == '__main__':
-    app.run()
-
+    app.run(threaded=True)
